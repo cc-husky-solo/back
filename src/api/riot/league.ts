3c0db7bb2aa4e4b24a2361626eb9fb8d5d8d12e0
Minetoma Gustavo <gminetoma@gmail.com>
2024-08-15 21:21:34 +0000

    Fix: TypeError: Cannot read properties of undefine When passing no summoner id, the server was trying to access the response but it was undefined.

---
+++ src/api/riot/league.ts
@@ -0,0 +1,32 @@
+import LeagueEntryDto from "../../interfaces/LeagueEntryDto";
+import { GetBySummonerId } from "../../models/league.model";
+
+const protocol = "https://";
+const path = ".api.riotgames.com/lol/league/v4/entries/";
+
+const RIOT_API_KEY = process.env.RIOT_API_KEY ?? "";
+
+export const getBySummonerId = async (
+  params: GetBySummonerId
+): Promise<LeagueEntryDto | null> => {
+  const route = "by-summoner/";
+
+  const completePath =
+    protocol + params.regionId.toLowerCase() + path + route + params.summonerId;
+
+  const config: RequestInit = {
+    headers: {
+      "X-Riot-Token": RIOT_API_KEY,
+    },
+  };
+
+  const response: LeagueEntryDto = (
+    await (await fetch(completePath, config)).json()
+  )[0];
+  if (!response) return null;
+  if (response.summonerId) return response;
+  else {
+    console.log(completePath, response);
+    return null;
+  }
+};
